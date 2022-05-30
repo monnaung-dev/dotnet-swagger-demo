db8b1d5faef3d901817e40bc9b3cceceb61c7d87
monnaung <monnaung.ny@gmail.com>
2022-05-30 11:57:36 +0630

    'commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SwaggerDemo.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<string> Get(int id)
+        {
+            return new string[] { "value1"};
+        }
+    }
+}
